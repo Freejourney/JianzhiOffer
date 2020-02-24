c8f17404c54b3745dadcf0f2adf8f0c0b29ff10d
Freejourney <983621131@qq.com>
2020-02-24 07:38:39 +0000

    added a new display function which can receive and display string parameter

--- src/SortMethods/MSort.java
+++ src/SortMethods/MSort.java
@@ -13,4 +13,12 @@ public abstract class MSort {
         }
         System.out.println();
     }
+
+    public void display(String str, int[] nums) {
+        System.out.print("-- "+str+" : ");
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i]+" ");
+        }
+        System.out.println();
+    }
 }
