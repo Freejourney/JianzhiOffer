fba7090874f173b56a271aab999278dcffadedcb
Freejourney <983621131@qq.com>
2020-02-27 11:06:15 +0000

    heap

--- src/SortMethods/Type2/BinaryHeap.java
+++ src/SortMethods/Type2/BinaryHeap.java
@@ -1,6 +1,8 @@
 package SortMethods.Type2;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * Created by ua28 on 2/25/20.
@@ -16,6 +18,21 @@ public class BinaryHeap {
             array[childIndex] = array[parentIndex];
             childIndex = parentIndex;
             parentIndex = (parentIndex - 1) / 2;
+        }
+        array[childIndex] = temp;
+    }
+
+    public void upAdjust_(int[] array) {
+        int childIndex = array.length - 1;
+        int parentIndex = (childIndex - 1) / 2;
+        int temp = array[childIndex];
+        while (childIndex > 0) {
+            if (array[parentIndex] < temp) {
+                break;
+            }
+            array[childIndex] = array[parentIndex];
+            childIndex = parentIndex;
+            parentIndex = (parentIndex - 1)/2;
         }
         array[childIndex] = temp;
     }
@@ -47,8 +64,59 @@ public class BinaryHeap {
         new BinaryHeap().upAdjust(array);
         System.out.println(Arrays.toString(array));
 
+        int level = getLevels(array.length);
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(0);
+        int num = 0;
+        int expectedNum = 1;
+        while (!queue.isEmpty()) {
+            int index = queue.remove();
+            System.out.print(array[index]+"  ");
+            num++;
+            if (2*index+1 < array.length)
+                queue.add(2*index+1);
+            if (2*index+2 < array.length)
+                queue.add(2*index+2);
+            if (num == expectedNum) {
+                System.out.println();
+                expectedNum *= 2;
+                num = 0;
+            }
+        }
+        System.out.println();
+
         int[] array2 = new int[] {7, 1, 3, 10, 5, 2, 8, 9, 6};
         new BinaryHeap().buildHeap(array2);
+
         System.out.println(Arrays.toString(array2));
+        Queue<Integer> queue2 = new LinkedList<>();
+        queue2.add(0);
+        int nums = 0;
+        int expectedNum2 = 1;
+        while (!queue2.isEmpty()) {
+            int index = queue2.remove();
+            System.out.print(array2[index]+"  ");
+            nums++;
+            if (2*index+1 < array2.length)
+                queue2.add(2*index+1);
+            if (2*index+2 < array2.length)
+                queue2.add(2*index+2);
+            if (nums == expectedNum2) {
+                expectedNum2 *= 2;
+                nums = 0;
+                System.out.println();
+            }
+        }
+    }
+
+    public static int getLevels(int length) {
+        if (length <= 0)
+            return 0;
+        int level = 1;
+        while (length != 1) {
+            length /= 2;
+            level++;
+        }
+        return level;
     }
 }
