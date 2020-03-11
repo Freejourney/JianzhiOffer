7b9c3b944921173608c6b1b7ad868df2c80d60af
Freejourney <983621131@qq.com>
2020-03-11 13:04:55 +0000

    added up2down method and down2up method for judging balanced binary tree

--- src/_7_16_BinaryTree_IsBalanced.java
+++ src/_7_16_BinaryTree_IsBalanced.java
@@ -3,6 +3,14 @@
  */
 public class _7_16_BinaryTree_IsBalanced implements TreeTest {
 
+    /**
+     * Balanced Binary Tree
+     *
+     * get depth of nodes from root to leaves
+     *
+     * @param root
+     * @return
+     */
     public boolean isBalanced_Up2Down(BinaryTreeNode root) {
         if (root == null)
             return false;
@@ -16,8 +24,43 @@ public class _7_16_BinaryTree_IsBalanced implements TreeTest {
         return isBalanced_Up2Down(root.left) && isBalanced_Up2Down(root.right);
     }
 
+
+    private boolean res = true;
+    /**
+     * Balanced Binary Tree
+     *
+     * get depth of nodes from leaves to root
+     *
+     * @param root
+     * @return
+     */
     public boolean isBalanced_Down2Up(BinaryTreeNode root) {
-        return false;
+       if (root == null)
+           return false;
+
+        isBalancedHelper(root);
+
+        return res;
+    }
+
+    /**
+     * fuse getDepth method with comparing depth process
+     * the function can compare left depth with right depth
+     * when nodes calculate depth from leaves to root
+     * @param root
+     * @return
+     */
+    private int isBalancedHelper(BinaryTreeNode root) {
+        if (root == null)
+            return 0;
+
+        int left = isBalancedHelper(root.left) + 1;
+        int right = isBalancedHelper(root.right) + 1;
+
+        if (Math.abs(left - right) > 1)
+            res = false;
+
+        return left > right ? left : right;
     }
 
     public int getDepth(BinaryTreeNode root) {
@@ -32,12 +75,20 @@ public class _7_16_BinaryTree_IsBalanced implements TreeTest {
 
     @Override
     public BinaryTreeNode buildTree() {
-        return null;
+        BinaryTreeNode root = new BinaryTreeNode(1);
+        BinaryTreeNode node2 = new BinaryTreeNode(2);
+        BinaryTreeNode node3 = new BinaryTreeNode(3);
+
+        root.left = node2;
+        node2.right = node3;
+
+        return root;
     }
 
     @Override
     public void test() {
-        isBalanced_Up2Down(buildTree());
+        System.out.println(isBalanced_Up2Down(buildTree()));
+        System.out.println(isBalanced_Down2Up(buildTree()));
     }
 
     public static void main(String[] args) {
