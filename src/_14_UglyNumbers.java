4fe4ede4633d41c52f37bd8cb361f8a93d020819
Freejourney <983621131@qq.com>
2020-03-18 15:02:01 +0000

    added find nth ugly number with method calculating from 1th to nth

--- src/_14_UglyNumbers.java
+++ src/_14_UglyNumbers.java
@@ -24,6 +24,46 @@ public class _14_UglyNumbers {
     }
 
     /**
+     * a ugly number multiplies 2, 3, 5 is still a ugly number
+     * calculate every ugly number from 1th to nth
+     * @param n
+     * @return
+     */
+    public int getNthUglyNumber_FromDownToUp(int n) {
+        if (n <= 0)
+            return -1;
+
+        int[] uglyNumbers = new int[n];
+        uglyNumbers[0] = 1;
+        int nextUglyNumber = 1;
+
+        int pMultiply2 = 0, pMultiply3 = 0, pMultiply5 = 0;
+
+        while (nextUglyNumber < n) {
+            int min = Min(uglyNumbers[pMultiply2]*2, uglyNumbers[pMultiply3]*3, uglyNumbers[pMultiply5]*5);
+            uglyNumbers[nextUglyNumber] = min;
+
+            while (uglyNumbers[pMultiply2]*2 <= uglyNumbers[nextUglyNumber])
+                ++pMultiply2;
+            while (uglyNumbers[pMultiply3]*3 <= uglyNumbers[nextUglyNumber])
+                ++pMultiply3;
+            while (uglyNumbers[pMultiply5]*5 <= uglyNumbers[nextUglyNumber])
+                ++pMultiply5;
+
+            nextUglyNumber++;
+        }
+
+        return uglyNumbers[nextUglyNumber-1];
+    }
+
+    private int Min(int num1, int num2, int num3) {
+        if (num1 < num2) {
+            return num1 < num3 ? num1 : num3;
+        }
+        return num2 < num3 ? num2 : num3;
+    }
+
+    /**
      * constantly divide certain factors until number becomes 1
      * @param number
      * @return
@@ -40,6 +80,7 @@ public class _14_UglyNumbers {
 
     public void test() {
         System.out.println(getNthUglyNumber(1500));
+        System.out.println(getNthUglyNumber_FromDownToUp(1500));
     }
 
     public static void main(String[] args) {
