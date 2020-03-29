aec024c8899eba09eb47f65b9e119f68399cf6b7
Freejourney <983621131@qq.com>
2020-03-29 13:47:18 +0000

    added find k min elements

---
+++ src/_18_MinKNum.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+/**
+ * Created by ua28 on 3/29/20.
+ */
+public class _18_MinKNum {
+
+    /**
+     * 时间复杂度为O(kn), 空间复杂度为O(1)
+     * 每次从剩余的序列中寻找最小的数，采用集合来过滤掉已经选取过得元素下标
+     * @param arr
+     * @param k
+     * @return
+     */
+    public int[] minKNum_onebyone(int[] arr, int k) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int j = 0; j < k; j++) {
+            int t = 0;
+            while (set.contains(t))
+                t++;
+            int min = arr[t];
+            int index = t;
+            for (int i = 0; i < arr.length; i++) {
+                if (min > arr[i]) {
+                    if (!set.contains(i)) {
+                        min = arr[i];
+                        index = i;
+                    }
+                }
+            }
+            set.add(index);
+        }
+
+        int[] result = new int[k];
+        int t = 0;
+        Iterator<Integer> iterator = set.iterator();
+        while (iterator.hasNext()) {
+            int i = iterator.next();
+            result[t++] = arr[i];
+        }
+        return result;
+    }
+
+    public void test() {
+        int[] arr = new int[]{4, 5, 1, 1, 0, 6, 6, 2, 7, 3, 8};
+        int k = 9;
+
+        int[] result = minKNum_onebyone(arr, k);
+        Arrays.sort(result);
+        for (int e : result)
+            System.out.print(e+" ");
+    }
+
+    public static void main(String[] args) {
+        new _18_MinKNum().test();
+    }
+}
